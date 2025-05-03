039548aed460b0e5ced2ee40a3dc245d174a8788
natalie-a-1 <natalie.a.hill-1@ou.edu>
2025-05-03 01:15:06 -0500

    feat: Add test setup file for Mocha tests and update test command path

---
+++ frontend/test/setup.ts
@@ -0,0 +1,8 @@
+// frontend/test/setup.ts
+
+// Setup file for frontend Mocha tests.
+// Requires ts-node/register has been loaded before this file.
+// You can import and configure global testing utilities here.
+// Currently, no setup code is required.
+
+export {}; 
